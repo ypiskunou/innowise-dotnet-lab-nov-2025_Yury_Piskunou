5dee8eef3d65636371fa311b12534a349f1cebe4
Yury Piskunou <ypiskunou@gmail.com>
2025-11-05 16:04:15 +0300

    init: tests

---
+++ Inno_Shop/tests/ProductService.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace ProductService.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Inno_Shop/tests/UserService.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace UserService.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
