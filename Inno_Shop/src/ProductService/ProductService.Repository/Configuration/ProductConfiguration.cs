804375c38507cb7766481b5c4ed2797551d04b87
Yury Piskunou <ypiskunou@gmail.com>
2025-12-01 00:29:20 +0300

    feat: Add Index for user emails and names, new Migration

--- Inno_Shop/src/ProductService/ProductService.Repository/Configuration/ProductConfiguration.cs
+++ Inno_Shop/src/ProductService/ProductService.Repository/Configuration/ProductConfiguration.cs
@@ -10,18 +10,39 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
     {
         builder.HasOne(p => p.Category)
             .WithMany(c => c.Products)
-            .HasForeignKey(p => p.CategoryId); 
+            .HasForeignKey(p => p.CategoryId);
         
-        builder.HasIndex(p => p.CategoryId)
-            .HasDatabaseName("IX_Products_CategoryId");
-        builder.HasIndex(p => p.UserId)
-            .HasDatabaseName("IX_Products_UserId");
+        builder.HasIndex(p => p.CategoryId).HasDatabaseName("IX_Products_CategoryId");
+        builder.HasIndex(p => p.UserId).HasDatabaseName("IX_Products_UserId");
         builder.HasIndex(p => p.Name);
-        
         builder.HasQueryFilter(p => !p.IsDeleted && p.IsActive);
         builder.Navigation(p => p.Category).AutoInclude();
-        
+
         builder.HasData(
+            new Product
+            {
+                Id = new Guid("8b007a4c-b9d0-4508-b233-ba47ffeb185f"),
+                Name = "Asus K501-UW",
+                Description = "Modern cut edge powerfull notebook",
+                Price = 1000m,
+                IsActive = true,
+                IsDeleted = false,
+                DateOfCreation = DateTime.Parse("2025-11-29T19:34:28.328986Z").ToUniversalTime(),
+                UserId = new Guid("80abbca8-664d-4b20-b5de-024705497d4a"), 
+                CategoryId = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870")
+            },
+            new Product
+            {
+                Id = new Guid("6a1a626b-8b48-42c5-8107-99a2a0e517fe"),
+                Name = "Samsung RV513",
+                Description = "Old low budget notebook",
+                Price = 100m,
+                IsActive = true,
+                IsDeleted = false,
+                DateOfCreation = DateTime.Parse("2025-11-29T19:36:23.966282Z").ToUniversalTime(),
+                UserId = new Guid("80abbca8-664d-4b20-b5de-024705497d4a"), 
+                CategoryId = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870")
+            },
             new Product
             {
                 Id = new Guid("4145bbe3-5e1e-4034-9d54-da63f56c02c3"),
@@ -30,9 +51,8 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
                 Price = 1299.99m,
                 IsActive = true,
                 IsDeleted = false,
-                DateOfCreation = DateTime.UtcNow,
-                DateOfLastUpdate = DateTime.UtcNow,
-                UserId = new Guid("86dba8c0-d178-41e7-938c-ed49778fb52a"),
+                DateOfCreation = DateTime.Parse("2025-11-30T01:22:06.517691Z").ToUniversalTime(),
+                UserId = new Guid("80abbca8-664d-4b20-b5de-024705497d4a"), 
                 CategoryId = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870")
             },
             new Product
@@ -43,10 +63,21 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
                 Price = 49.99m,
                 IsActive = true,
                 IsDeleted = false,
-                DateOfCreation = DateTime.UtcNow,
-                DateOfLastUpdate = DateTime.UtcNow,
+                DateOfCreation = DateTime.Parse("2025-11-30T01:22:06.517692Z").ToUniversalTime(),
                 UserId = new Guid("86dba8c0-d178-41e7-938c-ed49778fb52a"),
                 CategoryId = new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3")
+            },
+            new Product
+            {
+                Id = new Guid("76612c72-c934-4a97-8195-78a85badb03e"),
+                Name = "Test Phone",
+                Description = "Test desc",
+                Price = 100m,
+                IsActive = true,
+                IsDeleted = true, 
+                DateOfCreation = DateTime.Parse("2025-11-24T04:57:27.431701Z").ToUniversalTime(),
+                UserId = new Guid("f2b8dd4e-bb69-41ce-8fd6-f213790a460b"), 
+                CategoryId = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870")
             }
         );
     }
--- Inno_Shop/src/UserService/UserService.Repository/Configuration/UserConfiguration.cs
+++ Inno_Shop/src/UserService/UserService.Repository/Configuration/UserConfiguration.cs
@@ -9,20 +9,38 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
+        builder.HasIndex(u => u.Email)
+            .IsUnique(); 
+        
+        builder.HasIndex(u => u.Name);
+        
         builder.HasData(
             new User
             {
                 Id = new Guid("80abbca8-664d-4b20-b5de-024705497d4a"),
                 Name = "Admin User",
                 Email = "admin@example.com",
-                PasswordHash = "AQAAAAIAAYagAAAAEIIzHBkWv7JytQET6e5qtoNMQpXngwmkQwoOru9yImtfeGzabBmFk0gK3Tpq5sgogg"
+                PasswordHash = "AQAAAAIAAYagAAAAEGtVOXkmwwc57KzDGVK1Y8M8GCICFLFvDpaDilkYvr48jPPbt1vOxyTeCUCD0s6p+Q==",
+                VerifiedAt = DateTime.UtcNow, 
+                IsActive = true
             },
             new User
             {
-                Id = new Guid("86dba8c0-d178-41e7-938c-ed49778fb52a"),
+                Id = new Guid("84fe1970-e510-45c3-90a8-8e0607d056e9"),
                 Name = "Regular User",
                 Email = "user@example.com",
-                PasswordHash = "AQAAAAIAAYagAAAAEHxYENv88wq8F6SIbaWKZMBOM+FdQlbzej9N0xNpX5I8fHdSUH+Lv1sEWe7EJxhe5Q=="
+                PasswordHash = "AQAAAAIAAYagAAAAEHP8SZ2ACcbxZChtJd4YrJW3H1hfZYbUpWAeifxw6heZoZpI9PJcs+Ks5S0ul4ryCQ==",
+                VerifiedAt = DateTime.UtcNow,
+                IsActive = true
+            },
+            new User
+            {
+                Id = new Guid("f2b8dd4e-bb69-41ce-8fd6-f213790a460b"),
+                Name = "Anton Volchkov",
+                Email = "volchkov@inno.by",
+                PasswordHash = "AQAAAAIAAYagAAAAEGzGIA5pP3SUaaSHYI093ON4tjYMcLdcY3MDAXB0CwtQIENCmU8jSxEqRfc4ze8XUg==",
+                VerifiedAt = DateTime.UtcNow,
+                IsActive = false 
             }
         );
     }
